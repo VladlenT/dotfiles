3dadc1323122a5a3eac8e279486af31cfd9fff56
Vladlen Tereshchenko <vladlent.dev@gmail.com>
2021-01-05 16:34:26 +0200

    (config.sh) Added comments

--- config.sh
+++ config.sh
@@ -1,8 +1,19 @@
+# Set default terminal mode to 'vi'
 set -o vi
+
+# Remove all default aliases
 unalias -a
+
+# Change umask to make directory sharing easier
+umask 0002
+
 alias cl=clear
 alias ll="ls -la"
 alias vi=vim
 alias ?=duckduckgo
+
+# Ignore duplicates in command history
+export HISTCONTROL=ignoredups
+
 export PATH=/mnt/sda1/repos/github.com/VladlenT/dotfiles/scripts:$PATH
 
